db4923f30a2dda94d52273ad1c1d8184ae5ccd5b
tduplantis83 <tduplantis83@gmail.com>
2019-09-15 17:19:46 -0600

    Added more methods for efficiency and organizational value.

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -9,20 +9,13 @@ public class MakeChangeApp {
 		// formats the change into a money String
 		NumberFormat formatter = NumberFormat.getCurrencyInstance();
 
-				registerWelcome();
+			registerWelcome();
 			itemPrice = getItemPrice(input);
 			amtTendered = getAmtTendered(input);
 			amtTendered = checkAmtTendered(amtTendered, itemPrice, formatter, input);
 			
 			if(amtTendered > 0) {
-				change = Double.parseDouble(formatter.format(makeChange(amtTendered, itemPrice)).substring(1,
-						formatter.format(makeChange(amtTendered, itemPrice)).length()));
-				
-				System.out.println("*****************************************");
-				System.out.println("Item Price: " + formatter.format(itemPrice));
-				System.out.println("Amount Tendered: " + formatter.format(amtTendered));
-				System.out.println("Your change is: " + formatter.format(change));
-				buildChangeString(change);
+				makeChange(amtTendered, itemPrice, formatter);
 			}
 			else {
 				System.out.println("Take your money back and get out of my store. NO SOUP FOR YOU!");
@@ -31,151 +24,196 @@ public class MakeChangeApp {
 		input.close();
 	}
 
-	private static void buildChangeString(double change) {
+	private static void buildChangeString(int hundreds, int fifties, int twenties, int tens, int fives, int ones, int fiftyCents, int quarters, int dimes, int nickels, int pennies, String c, String price, String amtPaid) {
 		// take in change (double)
 		// determine the dollars & coins needed
 		// print out the results for user as a string
 		// Bill types:
+		
+		// build String to list out change
+		String changeString = "";
+		
+		//build string of denominations for payout
+		//hundreds
+		if (hundreds > 1) {
+			changeString += "\t" + hundreds + " $100 bills\n";
+		} else if (hundreds == 1) {
+			changeString += "\t" + hundreds + " $100 bill\n";
+		}
+
+		if (fifties > 1) {
+			changeString += "\t" + fifties + " $50 bills\n";
+		} else if (fifties == 1) {
+			changeString += "\t" + fifties + " $50 bill\n";
+		}
+		
+		// twenties
+		if (twenties > 1) {
+			changeString += "\t" + twenties + " $20 bills\n";
+		} else if (twenties == 1) {
+			changeString += "\t" + twenties + " $20 bill\n";
+		}
+		
+		// tens
+		if (tens > 1) {
+			changeString += "\t" + tens + " $10 bills\n";
+		} else if (tens == 1) {
+			changeString += "\t" + tens + " $10 bill\n";
+		}
+
+		// fives
+		if (fives > 1) {
+			changeString += "\t" + fives + " $5 bills\n";
+		} else if (fives == 1) {
+			changeString += "\t" + fives + " $5 bill\n";
+		}
+		
+		// ones
+		if (ones > 1) {
+			changeString += "\t" + ones + " $1 bills\n";
+		} else if (ones == 1) {
+			changeString += "\t" + ones + " $1 bill\n";
+		}
+		
+
+		// find coins
+		// 50cents
+		if (fiftyCents > 1) {
+			changeString += "\t" + fiftyCents + " Fifty Cent Pieces\n";
+		} else if (fiftyCents == 1) {
+			changeString += "\t" + fiftyCents + " Fifty Cent Piece\n";
+
+		}
+		
+		// quarters
+		if (quarters > 1) {
+			changeString += "\t" + quarters + " Quarters\n";
+		} else if (quarters == 1) {
+			changeString += "\t" + quarters + " Quarter\n";
+		}
+		
+		// dimes
+		if (dimes > 1) {
+			changeString += "\t" + dimes + " Dimes\n";
+		} else if (dimes == 1) {
+			changeString += "\t" + dimes + " Dime\n";
+		}
+		
+		// nickels
+		if (nickels > 1) {
+			changeString += "\t" + nickels + " Nickels\n";
+		} else if (nickels == 1) {
+			changeString += "\t" + nickels + " Nickel\n";
+		}
+		
+		// pennies
+		if (pennies > 1) {
+			changeString += "\t" + pennies + " Pennies";
+		} else if (pennies == 1) {
+			changeString += "\t" + pennies + " Penny";
+		}
+
+		System.out.println("*******************************");
+		System.out.println("Item Price: \t\t" + price);
+		System.out.println("Amount Tendered: \t" + amtPaid);
+		System.out.println("Your change is: \t" + c);
+		System.out.println(changeString);
+		System.out.println("*******************************");
+		System.out.println("Thank you! Come Again!");
+
+	}
+
+	private static int findLargestDenominations(int cents, int denom) {
+		int countOfDenom = 0;
+		
+		// find denominations
+		if (cents / denom >= 0) {
+			countOfDenom = cents / denom;
+		}
+		
+		return countOfDenom;
+	}
+	
+	private static void makeChange(double amt, double price, NumberFormat nf) {
+		// take in the amount paid (double)
+		// take in the item price (double)
+		// determine the change due (double)
+		// convert change due to cents
+		// call findLargestDenominations method to find all denominations 
+		// call buildChangeString method to build a string of the denominations needed for payout
+		
+		double change = amt - price;
+		
 		// $100, $50, $20, $10, $5, $1
 		int hundreds = 0, fifties = 0, twenties = 0, tens = 0, fives = 0, ones = 0;
 
 		// Coin types:
 		// 50 cent, 25 cent, 10 cent, 5 cent, 1 cent
 		int fiftyCents = 0, quarters = 0, dimes = 0, nickels = 0, pennies = 0;
-
-		// build String to list out change
-		String changeString = "";
-
-		// convert change to cents
+		
+		// convert change to currency format to account for double rounding errors
+		// then convert change to a double with rounding error fixed
+		// then convert change into cents
+		change = Double.parseDouble(nf.format(change).substring(1, (nf.format(change).length())));
 		int cents = (int) (change * 100);
 
-		// find bills
-		// hundreds
-		if (cents / 10000 >= 0) {
-			hundreds = cents / 10000;
-			cents -= hundreds * 10000;
-			if (hundreds > 1) {
-				changeString += "\t" + hundreds + " $100 bills\n";
-			} else if (hundreds == 1) {
-				changeString += "\t" + hundreds + " $100 bill\n";
-			}
-		}
+		//hundreds
+		hundreds = findLargestDenominations(cents, 10000);
+		//update cents
+		cents -= hundreds * 10000;
+				
 		// fifties
-		if (cents / 5000 >= 0) {
-			fifties = cents / 5000;
-			cents -= fifties * 5000;
-			if (fifties > 1) {
-				changeString += "\t" + fifties + " $50 bills\n";
-			} else if (fifties == 1) {
-				changeString += "\t" + fifties + " $50 bill\n";
-			}
-		}
+		fifties = findLargestDenominations(cents, 5000);
+		//update cents
+		cents -= fifties * 5000;
+		
 		// twenties
-		if (cents / 2000 >= 0) {
-			twenties = cents / 2000;
-			cents -= twenties * 2000;
-			if (twenties > 1) {
-				changeString += "\t" + twenties + " $20 bills\n";
-			} else if (twenties == 1) {
-				changeString += "\t" + twenties + " $20 bill\n";
-			}
-		}
+		twenties = findLargestDenominations(cents, 2000);
+		//update cents
+		cents -= twenties * 2000;
+		
 		// tens
-		if (cents / 1000 >= 0) {
-			tens = cents / 1000;
-			cents -= tens * 1000;
-			if (tens > 1) {
-				changeString += "\t" + tens + " $10 bills\n";
-			} else if (tens == 1) {
-				changeString += "\t" + tens + " $10 bill\n";
-			}
-		}
+		tens = findLargestDenominations(cents, 1000);
+		//update cents
+		cents -= tens * 1000;
+		
 		// fives
-		if (cents / 500 >= 0) {
-			fives = cents / 500;
-			cents -= fives * 500;
-			if (fives > 1) {
-				changeString += "\t" + fives + " $5 bills\n";
-			} else if (fives == 1) {
-				changeString += "\t" + fives + " $5 bill\n";
-			}
-		}
+		fives = findLargestDenominations(cents, 500);
+		//update cents
+		cents -= fives * 500;
+		
 		// ones
-		if (cents / 100 >= 0) {
-			ones = cents / 100;
-			cents -= ones * 100;
-			if (ones > 1) {
-				changeString += "\t" + ones + " $1 bills\n";
-			} else if (ones == 1) {
-				changeString += "\t" + ones + " $1 bill\n";
-			}
+		ones = findLargestDenominations(cents, 100);
+		//update cents
+		cents -= ones * 100;
+		
+		// .50
+		fiftyCents = findLargestDenominations(cents, 50);
+		//update cents
+		cents -= fiftyCents * 50;
+		
+		// .25
+		quarters = findLargestDenominations(cents, 25);
+		//update cents
+		cents -= quarters * 25;
+		
+		// .10
+		dimes = findLargestDenominations(cents, 10);
+		//update cents
+		cents -= dimes * 10;
+		
+		// .05
+		nickels = findLargestDenominations(cents, 5);
+		//update cents
+		cents -= nickels * 5;
+		
+		// .01
+		pennies = findLargestDenominations(cents, 1);
+		//update cents
+		cents -= pennies * 1;
+		
+		buildChangeString(hundreds, fifties, twenties, tens, fives, ones, fiftyCents, quarters, dimes, nickels, pennies, nf.format(change), nf.format(price), nf.format(amt));
 		}
-
-		// find coins
-		// 50cents
-		if (cents / 50 >= 0) {
-			fiftyCents = cents / 50;
-			cents -= fiftyCents * 50;
-			if (fiftyCents > 1) {
-				changeString += "\t" + fiftyCents + " Fifty Cent Pieces\n";
-			} else if (fiftyCents == 1) {
-				changeString += "\t" + fiftyCents + " Fifty Cent Piece\n";
-
-			}
-		}
-		// quarters
-		if (cents / 25 >= 0) {
-			quarters = cents / 25;
-			cents -= quarters * 25;
-			if (quarters > 1) {
-				changeString += "\t" + quarters + " Quarters\n";
-			} else if (quarters == 1) {
-				changeString += "\t" + quarters + " Quarter\n";
-			}
-		}
-		// dimes
-		if (cents / 10 >= 0) {
-			dimes = cents / 10;
-			cents -= dimes * 10;
-			if (dimes > 1) {
-				changeString += "\t" + dimes + " Dimes\n";
-			} else if (dimes == 1) {
-				changeString += "\t" + dimes + " Dime\n";
-			}
-		}
-		// nickels
-		if (cents / 5 >= 0) {
-			nickels = cents / 5;
-			cents -= nickels * 5;
-			if (nickels > 1) {
-				changeString += "\t" + nickels + " Nickels\n";
-			} else if (nickels == 1) {
-				changeString += "\t" + nickels + " Nickel\n";
-			}
-		}
-		// pennies
-		if (cents / 1 >= 0) {
-			pennies = cents / 1;
-			cents -= pennies * 1;
-			if (pennies > 1) {
-				changeString += "\t" + pennies + " Pennies";
-			} else if (pennies == 1) {
-				changeString += "\t" + pennies + " Penny";
-			}
-		}
-
-		System.out.println(changeString);
-		System.out.println("Thank you! Come Again!");
-
-	}
-
-	private static double makeChange(double amt, double price) {
-		// take in the amount paid (double)
-		// take in the item price (double)
-		// return the change due (double)
-
-		return amt - price;
-	}
 
 	private static double checkAmtTendered(double amt, double price, NumberFormat nf, Scanner i) {
 
